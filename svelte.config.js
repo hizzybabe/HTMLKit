fc7b6733d3a411018f777628c9329e9006725085
hizzybabe <napipat_s@hotmail.com>
2024-11-02 01:41:15 +0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      pages: '.svelte-kit/output',
-      assets: '.svelte-kit/output',
-      fallback: '404.html',
-      precompress: false,
-      strict: true
+      fallback: 'index.html'
     }),
     paths: {
       base: '/my-svelte-website'
