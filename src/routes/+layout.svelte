1c1ebda0dbde81eabbe308e8cfbac17bed0b3b7e
hizzybabe <napipat_s@hotmail.com>
2024-11-02 15:35:21 +0700

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <nav>
   <a href="/my-svelte-website/">Home</a>
   <a href="/my-svelte-website/about">About</a>
-  <a href="/my-svelte-website/projects">Projects</a>
+  <a href="/my-svelte-website/projects">HTML</a>
+  <a href="/my-svelte-website/projects">CSS</a>
+  <a href="/my-svelte-website/projects">Javascript</a>
+  <a href="/my-svelte-website/projects">Svelte</a>
+  <a href="/my-svelte-website/projects">Astro</a>
   <a href="/my-svelte-website/practice">Practice</a>
 </nav>
 
