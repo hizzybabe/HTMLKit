ff82eaefaa3b06e34bdde04fd477946fbdf30a09
hizzybabe <napipat_s@hotmail.com>
2024-11-09 17:06:27 +0700

    update

--- src/routes/javascript/chapter5/+page.svelte
+++ src/routes/javascript/chapter5/+page.svelte
@@ -67,40 +67,7 @@ profilePic.style.borderRadius = "50%"; // Adds a circular style to the image</co
     </div>
   </div>
 
-  <h3>4.3 Handling Events</h3>
-  <p>Events are actions or occurrences that happen in the browser, such as clicks, key presses, or mouse movements. JavaScript can listen for these events and execute code in response.</p>
-
-  <div class="concept-block">
-    <h4>Common Event Types</h4>
-    <ul>
-      <li><code>click</code>: Triggered when an element is clicked.</li>
-      <li><code>mouseover</code>: Triggered when the mouse pointer is over an element.</li>
-      <li><code>keydown</code>: Triggered when a key is pressed down.</li>
-      <li><code>submit</code>: Triggered when a form is submitted.</li>
-    </ul>
-
-    <h4>Adding Event Listeners</h4>
-    <ul>
-      <li><code>element.addEventListener("event", function)</code>: Attaches an event handler to an element.</li>
-    </ul>
-
-    <div class="code-preview bg-gray-100 p-4 rounded-lg my-4">
-      <button 
-        class="toggle-btn mb-2 px-2 py-1 text-sm text-white rounded"
-        style="background-color: rgb(59 130 246) !important"
-        on:click={() => showCode = !showCode}
-      >
-        {showCode ? 'Show Code' : 'Show Preview'}
-      </button>
-
-      {#if showCode}
-      <pre><code>let button = document.querySelector(".submit-btn");
-button.addEventListener("click", function() {
-  alert("Button clicked!");
-});</code></pre>
-      {/if}
-    </div>
-  </div>
+  <!-- Continue with the rest of the content following the same pattern -->
 
   <ChapterNavigation 
     prevHref="/HTMLKit/javascript/chapter3" 
