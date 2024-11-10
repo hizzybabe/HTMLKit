0241f862116a37af5e6a5a7087fee7b5cdaec9c9
hizzybabe <napipat_s@hotmail.com>
2024-11-10 17:35:54 +0700

    update

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -2,20 +2,26 @@
 
 <div class="container mx-auto px-4">
   <section>
-    <h2>HTML Projects</h2>
+    <h2>HTML, CSS Projects</h2>
     <ul>
-      <li><a href="/my-svelte-website/practice/html-form">Registration Form</a></li>
-      <li><a href="/my-svelte-website/practice/html-table">Data Table</a></li>
-      <li><a href="/my-svelte-website/practice/html-semantic">Semantic Layout</a></li>
-    </ul>
-  </section>
-
-  <section>
-    <h2>CSS Projects</h2>
-    <ul>
-      <li><a href="/my-svelte-website/practice/css-grid">Grid Gallery</a></li>
-      <li><a href="/my-svelte-website/practice/css-animation">Animations</a></li>
-      <li><a href="/my-svelte-website/practice/css-responsive">Responsive Design</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss1">Personal Portfolio</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss2">Single-Page Resume</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss3">Landing Page for a Product</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss4">Photo Gallery</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss5">Recipe Card</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss6">Pricing Table</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss7">Newsletter Signup Form</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss8">CSS-Only Navigation Menu</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss9">Restaurant Menu</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss10">Simple Blog Post Layout</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss11">Product Card</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss12">Event Invitation Page</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss13">Digital Business Card</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss14">Simple Portfolio Grid</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss15">CSS Art</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss16">Weather Card</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss17">User Profile Page</a></li>
+      <li><a href="/my-svelte-website/practice/htmlcss18">Coming Soon Page</a></li>
     </ul>
   </section>
 
@@ -25,15 +31,34 @@
       <li><a href="/my-svelte-website/practice/js-todo">Todo List</a></li>
       <li><a href="/my-svelte-website/practice/js-quiz">Quiz App</a></li>
       <li><a href="/my-svelte-website/practice/js-calculator">Calculator</a></li>
+      <li><a href="/my-svelte-website/practice/js-countdown">Countdown Timer</a></li>
+      <li><a href="/my-svelte-website/practice/js-weather">Weather App</a></li>
+      <li><a href="/my-svelte-website/practice/js-rps">Rock, Paper, Scissors Game</a></li>  
+      <li><a href="/my-svelte-website/practice/js-quote">Random Quote Generator</a></li>
+      <li><a href="/my-svelte-website/practice/js-stopwatch">Stopwatch</a></li>
+      <li><a href="/my-svelte-website/practice/js-memory">Memory Game</a></li>  
+      <li><a href="/my-svelte-website/practice/js-expense">Expense Tracker</a></li>
+      <li><a href="/my-svelte-website/practice/js-typing">Typing Speed Test</a></li>
+      <li><a href="/my-svelte-website/practice/js-recipe">Recipe Finder</a></li>
+      <li><a href="/my-svelte-website/practice/js-temperature">Temperature Converter</a></li>
+      <li><a href="/my-svelte-website/practice/js-drum">Drum Kit</a></li>
+      <li><a href="/my-svelte-website/practice/js-clock">Digital Clock</a></li>
+      <li><a href="/my-svelte-website/practice/js-color">Color Flipper</a></li>
+      <li><a href="/my-svelte-website/practice/js-survey">Survey Form</a></li>
+      <li><a href="/my-svelte-website/practice/js-carousel">Image Carousel</a></li>
+      <li><a href="/my-svelte-website/practice/js-password">Password Generator</a></li>
+      <li><a href="/my-svelte-website/practice/js-drawing">Simple Drawing App</a></li>
+      <li><a href="/my-svelte-website/practice/js-modal">Popup Modal</a></li>
+      <li><a href="/my-svelte-website/practice/js-accordion">Accordion FAQ Section</a></li>
     </ul>
   </section>
 
   <section>
-    <h2>Svelte Projects</h2>
+    <h2>Astro Projects</h2>
     <ul>
-      <li><a href="/my-svelte-website/practice/svelte-counter">Counter App</a></li>
-      <li><a href="/my-svelte-website/practice/svelte-weather">Weather Widget</a></li>
-      <li><a href="/my-svelte-website/practice/svelte-crud">CRUD Operations</a></li>
+      <li><a href="/my-svelte-website/practice/astro-blog">Astro Blog</a></li>
+      <li><a href="/my-svelte-website/practice/astro-landing">Astro Landing Page</a></li>
+      <li><a href="/my-svelte-website/practice/astro-company">Astro Company Website</a></li>
     </ul>
   </section>
 </div>
