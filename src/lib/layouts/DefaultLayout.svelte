7fda28de8315ed106daf43c3798eedb5d5106b6c
hizzybabe <napipat_s@hotmail.com>
2024-11-10 22:43:57 +0700

    mdsvex

--- src/lib/layouts/DefaultLayout.svelte
+++ src/lib/layouts/DefaultLayout.svelte
@@ -1,10 +1,11 @@
 <script>
-  // These props will come from your markdown frontmatter
   export let title;
 </script>
 
-<article class="prose prose-slate max-w-3xl mx-auto px-4 py-8">
-  <h1>{title}</h1>
-  
-  <slot />
-</article> 
+<div class="container mx-auto px-4">
+  <article class="prose prose-slate max-w-3xl mx-auto py-8">
+    <h1>{title}</h1>
+    
+    <slot />
+  </article> 
+</div> 
