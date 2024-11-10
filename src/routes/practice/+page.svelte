162f6777eb897bd80bd2cd4a68137e05e012d3ac
hizzybabe <napipat_s@hotmail.com>
2024-11-10 22:33:06 +0700

    mdsvex

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -4,61 +4,61 @@
   <section>
     <h2>HTML, CSS Projects</h2>
     <ul>
-      <li><a href="/my-svelte-website/practice/htmlcss1">Personal Portfolio</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss2">Single-Page Resume</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss3">Landing Page for a Product</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss4">Photo Gallery</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss5">Recipe Card</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss6">Pricing Table</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss7">Newsletter Signup Form</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss8">CSS-Only Navigation Menu</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss9">Restaurant Menu</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss10">Simple Blog Post Layout</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss11">Product Card</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss12">Event Invitation Page</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss13">Digital Business Card</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss14">Simple Portfolio Grid</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss15">CSS Art</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss16">Weather Card</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss17">User Profile Page</a></li>
-      <li><a href="/my-svelte-website/practice/htmlcss18">Coming Soon Page</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss1">Personal Portfolio</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss2">Single-Page Resume</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss3">Landing Page for a Product</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss4">Photo Gallery</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss5">Recipe Card</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss6">Pricing Table</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss7">Newsletter Signup Form</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss8">CSS-Only Navigation Menu</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss9">Restaurant Menu</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss10">Simple Blog Post Layout</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss11">Product Card</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss12">Event Invitation Page</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss13">Digital Business Card</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss14">Simple Portfolio Grid</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss15">CSS Art</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss16">Weather Card</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss17">User Profile Page</a></li>
+      <li><a href="/HTMLKit/practice/htmlcss18">Coming Soon Page</a></li>
     </ul>
   </section>
 
   <section>
     <h2>JavaScript Projects</h2>
     <ul>
-      <li><a href="/my-svelte-website/practice/js-todo">Todo List</a></li>
-      <li><a href="/my-svelte-website/practice/js-quiz">Quiz App</a></li>
-      <li><a href="/my-svelte-website/practice/js-calculator">Calculator</a></li>
-      <li><a href="/my-svelte-website/practice/js-countdown">Countdown Timer</a></li>
-      <li><a href="/my-svelte-website/practice/js-weather">Weather App</a></li>
-      <li><a href="/my-svelte-website/practice/js-rps">Rock, Paper, Scissors Game</a></li>  
-      <li><a href="/my-svelte-website/practice/js-quote">Random Quote Generator</a></li>
-      <li><a href="/my-svelte-website/practice/js-stopwatch">Stopwatch</a></li>
-      <li><a href="/my-svelte-website/practice/js-memory">Memory Game</a></li>  
-      <li><a href="/my-svelte-website/practice/js-expense">Expense Tracker</a></li>
-      <li><a href="/my-svelte-website/practice/js-typing">Typing Speed Test</a></li>
-      <li><a href="/my-svelte-website/practice/js-recipe">Recipe Finder</a></li>
-      <li><a href="/my-svelte-website/practice/js-temperature">Temperature Converter</a></li>
-      <li><a href="/my-svelte-website/practice/js-drum">Drum Kit</a></li>
-      <li><a href="/my-svelte-website/practice/js-clock">Digital Clock</a></li>
-      <li><a href="/my-svelte-website/practice/js-color">Color Flipper</a></li>
-      <li><a href="/my-svelte-website/practice/js-survey">Survey Form</a></li>
-      <li><a href="/my-svelte-website/practice/js-carousel">Image Carousel</a></li>
-      <li><a href="/my-svelte-website/practice/js-password">Password Generator</a></li>
-      <li><a href="/my-svelte-website/practice/js-drawing">Simple Drawing App</a></li>
-      <li><a href="/my-svelte-website/practice/js-modal">Popup Modal</a></li>
-      <li><a href="/my-svelte-website/practice/js-accordion">Accordion FAQ Section</a></li>
+      <li><a href="/HTMLKit/practice/js-todo">Todo List</a></li>
+      <li><a href="/HTMLKit/practice/js-quiz">Quiz App</a></li>
+      <li><a href="/HTMLKit/practice/js-calculator">Calculator</a></li>
+      <li><a href="/HTMLKit/practice/js-countdown">Countdown Timer</a></li>
+      <li><a href="/HTMLKit/practice/js-weather">Weather App</a></li>
+      <li><a href="/HTMLKit/practice/js-rps">Rock, Paper, Scissors Game</a></li>  
+      <li><a href="/HTMLKit/practice/js-quote">Random Quote Generator</a></li>
+      <li><a href="/HTMLKit/practice/js-stopwatch">Stopwatch</a></li>
+      <li><a href="/HTMLKit/practice/js-memory">Memory Game</a></li>  
+      <li><a href="/HTMLKit/practice/js-expense">Expense Tracker</a></li>
+      <li><a href="/HTMLKit/practice/js-typing">Typing Speed Test</a></li>
+      <li><a href="/HTMLKit/practice/js-recipe">Recipe Finder</a></li>
+      <li><a href="/HTMLKit/practice/js-temperature">Temperature Converter</a></li>
+      <li><a href="/HTMLKit/practice/js-drum">Drum Kit</a></li>
+      <li><a href="/HTMLKit/practice/js-clock">Digital Clock</a></li>
+      <li><a href="/HTMLKit/practice/js-color">Color Flipper</a></li>
+      <li><a href="/HTMLKit/practice/js-survey">Survey Form</a></li>
+      <li><a href="/HTMLKit/practice/js-carousel">Image Carousel</a></li>
+      <li><a href="/HTMLKit/practice/js-password">Password Generator</a></li>
+      <li><a href="/HTMLKit/practice/js-drawing">Simple Drawing App</a></li>
+      <li><a href="/HTMLKit/practice/js-modal">Popup Modal</a></li>
+      <li><a href="/HTMLKit/practice/js-accordion">Accordion FAQ Section</a></li>
     </ul>
   </section>
 
   <section>
     <h2>Astro Projects</h2>
     <ul>
-      <li><a href="/my-svelte-website/practice/astro-blog">Astro Blog</a></li>
-      <li><a href="/my-svelte-website/practice/astro-landing">Astro Landing Page</a></li>
-      <li><a href="/my-svelte-website/practice/astro-company">Astro Company Website</a></li>
+      <li><a href="/HTMLKit/practice/astro-blog">Astro Blog</a></li>
+      <li><a href="/HTMLKit/practice/astro-landing">Astro Landing Page</a></li>
+      <li><a href="/HTMLKit/practice/astro-company">Astro Company Website</a></li>
     </ul>
   </section>
 </div>
