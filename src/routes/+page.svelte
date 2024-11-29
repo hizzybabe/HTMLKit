0caa3f38b3fdc7904e9567068879e064d0787ab0
hizzybabe <napipat_s@hotmail.com>
2024-11-29 22:00:13 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,7 @@
 </div> -->
 
 <section class="latest-posts">
-  <h2 class="text-center">Latest Blog Posts</h2>
+  <h2>Latest Blog Posts</h2>
 
   {#await import.meta.glob('/src/routes/blog/**/*.md', { eager: true }) then modules}
     <div class="posts-grid">
