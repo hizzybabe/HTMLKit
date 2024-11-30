263baef9adcfe4b91b5cc4a56914cb9df8570ee4
hizzybabe <napipat_s@hotmail.com>
2024-11-30 19:59:39 +0700

    update

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -1,8 +1,17 @@
 export const prerender = true;
 
-export async function load({ fetch }) {
+export async function load({ url }) {
   const blogPosts = import.meta.glob('./**/*.md');
   
+  // For individual blog post pages
+  if (url.pathname !== '/blog') {
+    const resolved = await blogPosts[`.${url.pathname}/+page.md`]();
+    return {
+      metadata: resolved.metadata
+    };
+  }
+  
+  // For the blog index page
   const posts = await Promise.all(
     Object.entries(blogPosts).map(async ([path, resolver]) => {
       const resolved = await resolver();
